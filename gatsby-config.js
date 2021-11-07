6f0f8ffaa438edd69aabd574b352b691517effc2
gianclbal <gbaldonado@mail.sfsu.edu>
2021-11-06 21:36:06 -0700

    Added gatsby sharp, mdx, google fonts, and styled component plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,5 +43,31 @@ module.exports = {
         path: `${__dirname}/src/content/jobs`
       },
     },
+    `gatsby-plugin-sharp`, 
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `ibm plex sans` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
+    },
+    `gatsby-plugin-styled-components`,
   ],
 }
